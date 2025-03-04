ec70a6b9568d8b0dd0472659bdae428c54ce3aa7
Paul Mansfield <pmansfield@illumina.com>
2025-03-04 10:45:19 +0000

    trivial example of using google speech api

--- text_to_speech_with_google.sh
+++ text_to_speech_with_google.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mytext=$( echo $* | sed -e 's/ /+/g' )
+mytext=$( echo "$*" | sed -e 's/ /+/g' )
 
 vlc --play-and-exit -I dummy "http://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&tl=en&q=$mytext"
 
