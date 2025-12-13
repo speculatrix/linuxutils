4c4302d43988699925f6ce8e0a1084fc6796d5e6
Paul Mansfield <pmansfield@illumina.com>
2025-12-13 13:46:30 +0000

    helper wrapper

---
+++ output_change.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+FILE_OLD=$( mktemp /tmp/output_change_XXXXXX.old.txt )
+FILE_NEW=$( mktemp /tmp/output_change_XXXXXX.new.txt )
+
+CMD="$*"
+
+touch $FILE_OLD
+while /bin/date ; do
+	$CMD > "$FILE_NEW"
+	diff "$FILE_OLD" "$FILE_NEW"
+	sleep 2
+	cp -p "$FILE_NEW" "$FILE_OLD"
+done
+
