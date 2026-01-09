1b3629d8fa7c85c7f14db4a5d248c81fe414493b
Paul Mansfield <pmansfield@illumina.com>
2026-01-09 17:31:13 +0000

    improved the rolling date

--- output_change.sh
+++ output_change.sh
@@ -6,9 +6,13 @@ FILE_NEW=$( mktemp /tmp/output_change_XXXXXX.new.txt )
 CMD="$*"
 
 touch "$FILE_OLD"
-while /bin/date ; do
+while /bin/true ; do
+	/bin/date | tr -d "\n" 
+	echo -n "   "
 	$CMD > "$FILE_NEW"
+	#if cmp -s "$FILE_OLD" "$FILE_NEW" ; then
 	diff "$FILE_OLD" "$FILE_NEW"
+	#fi
 	sleep 2
 	cp -p "$FILE_NEW" "$FILE_OLD"
 done
