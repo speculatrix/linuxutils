9b158b6da4b71f7d0c04fa42b389896dde526641
Paul Mansfield <pmansfield@illumina.com>
2026-04-23 18:05:07 +0100

    tweaks to fix annoyances

--- ssh_agent_finder.sh
+++ ssh_agent_finder.sh
@@ -8,6 +8,7 @@
 # or if you need multiple agents with different keys loaded,
 # . /usr/local/bin/ssh_agent_finder <profile>
 
+#set -x
 
 # we should only output messages if on an interactive session,
 # otherwise it might upset ssh, scp or rsync
@@ -17,13 +18,13 @@ if [ $? -eq 1 ] ; then
 	ON_TTY=/bin/false
 fi
 
-# the profile simply changed the agent output file
+# the profile changes the agent output file
 if [ "$1" == "" ] ; then
 	agent_profile="default"
-	$ON_TTY && echo "Info, saving to default profile"
+	#$ON_TTY && echo "Info, using profile default"
 else
 	agent_profile="$1"
-	$ON_TTY && echo "Info, saving to $1 profile"
+	#$ON_TTY && echo "Info, using profile $1"
 fi
 
 
@@ -36,10 +37,13 @@ if [ "$SSH_AGENT_PROFILE" == "" ] ; then
 	export SSH_AGENT_PROFILE=default
 fi
 
-# ensure we save the default profile if it doesn't exist already
-if [ "$SSH_AGENT_PROFILE" == "default" ] && [ ! -f "$AGENTSAVEFILE" ] ; then
-	echo "Info, saving SSH env to $AGENTSAVEFILE"
-	env | grep SSH_				> "$AGENTSAVEFILE"
+# ensure we save the default profile if it doesn't exist already and we have SSH_AUTH_SOCK set
+if [ "$SSH_AGENT_PROFILE" == "default" ] && [ ! -f "$AGENTSAVEFILE" ] && [ "$SSH_AUTH_SOCK" != "" ]  ; then
+	echo "Info, creating/saving SSH env to $AGENTSAVEFILE"
+	env \
+		| grep SSH_ \
+		| grep -v -E "SSH_CONNECTION|SSH_CLIENT" \
+		> "$AGENTSAVEFILE"
 	echo "export SSH_AUTH_SOCK"		>> "$AGENTSAVEFILE"
 	echo "export SSH_AGENT_PID"		>> "$AGENTSAVEFILE"
 	echo "export SSH_AGENT_PROFILE"		>> "$AGENTSAVEFILE"
@@ -50,21 +54,27 @@ fi
 if [ "$SSH_AGENT_PROFILE" != "$agent_profile" ] ; then
 	echo "Info, switching to agent save file $AGENTSAVEFILE"
 	export SSH_AGENT_PROFILE="$agent_profile"
-	. "$AGENTSAVEFILE"
+	#. "$AGENTSAVEFILE"
+	. "$AGENTSAVEFILE" > /dev/null
 fi
 
 
 # when logging in on desktop, ssh-agent often already started for us and
 # SSH_AUTH_SOCK should already be set, if not try and find existing agent
 if [ "$SSH_AUTH_SOCK" == "" ] ; then
-	# if there's no saved agent details, start a new one
+	#echo "Debug, SSH_AUTH_SOCK not set"
 	if [ ! -f "$AGENTSAVEFILE" ] ; then
+		echo "Debug, there's no saved agent details, starting a new one"
 		$ON_TTY && echo "Info, SSH_AUTH_SOCK wasn't set, no recovery file, starting new agent"
-		ssh-agent > "$AGENTSAVEFILE"
+		ssh-agent | tee > "$AGENTSAVEFILE"
+	else
+		#echo "Debug, found and loading a saved agent file"
+		. "$AGENTSAVEFILE"
 	fi
 
 	# now load the agent details
 	# shellcheck disable=SC1090
+	#. "$AGENTSAVEFILE"
 	. "$AGENTSAVEFILE" > /dev/null
 	#$ON_TTY && echo "Debug, agent details 1: sock $SSH_AUTH_SOCK, pid $SSH_AGENT_PID"
 fi
@@ -74,17 +84,19 @@ if [ "$SSH_AUTH_SOCK" == "" ] ; then
 	# if SSH_AUTH_SOCK not set something went badly wrong
 	$ON_TTY && echo "Error, SSH_AUTH_SOCK still not set!"
 else
+	#env | grep SSH
 	# ssh-add returns 0 if working with keys, 1 if working but no keys, 2 if dead
 	ssh-add -l > /dev/null 2>&1
 	AGENT_COND=$?
+	#echo "AGENT_COND $AGENT_COND"
 	if [ $AGENT_COND -eq 0 ] ; then
 		#$ON_TTY && echo "Debug, the ssh agent was functional and has keys loaded"
 		: # need something if debug above is disabled
 	elif [ $AGENT_COND -eq 1 ] ; then
-		#$ON_TTY && echo "Debug, the ssh agent was functional but has no keys loaded"
+		#$ON_TTY && echo "Info, the ssh agent was functional but has no keys loaded"
 		: # need something if debug above is disabled
 	else
-		$ON_TTY && echo "Info, SSH_AUTH_SOCK pointed to defunct agent, starting a new one"
+		#$ON_TTY && echo "Info, SSH_AUTH_SOCK pointed to defunct agent, starting a new one"
 		ssh-agent > "$AGENTSAVEFILE"
 		# shellcheck disable=SC1090
 		. "$AGENTSAVEFILE" > /dev/null
@@ -92,3 +104,6 @@ else
 	fi
 fi
 
+set +x
+
+# end ssh_agent_finder.sh
